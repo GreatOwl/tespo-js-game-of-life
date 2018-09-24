fea37513a889c22293d7142f060d573c9528a152
Andrew Nowak <andrew@nowakfamily.net>
2018-09-24 14:15:47 -0400

    Test that user coordinates return corrected X base coordinate

--- src/spec/Coordinate.spec.ts
+++ src/spec/Coordinate.spec.ts
@@ -1,6 +1,6 @@
 import Coordinates from "../Coordinate.js";
 
-describe("Coordinates hold and operate as expected", function() {
+describe("Base Coordinate is reliable", function() {
     var expectedX:number;
     var expectedY:number = 7;
     it("expects getX to return 1", function() {
@@ -34,7 +34,6 @@ describe("Coordinates hold and operate as expected", function() {
         var coordinate = new Coordinates.Coordinate(4, 7);
         expect(coordinate.toString()).toBe(expectedString);
     });
-
     it("expects toString to return '0004400037'", function() {
         var expectedString:string = "0004400037";
         var coordinate = new Coordinates.Coordinate(44, 37);
@@ -50,5 +49,30 @@ describe("Coordinates hold and operate as expected", function() {
         var coordinate = new Coordinates.Coordinate(550044, 550037);
         expect(coordinate.toString()).toBe(expectedString);//.not(expectedString);
     });
-
   });
+describe("User Coordinate successfully converts to Base Coordinate", function() {
+    var userCoordinateRange:number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var expectedX:number;
+    var expectedY:number = 7;
+    var cursorFudgeSize: number = 10;
+    var size: number = userCoordinateRange.length;
+    var calculateUserCoordinate =  function(expected: number):number {
+        return expected * size + cursorFudgeSize
+    }
+    it("expects getX to return 1", function() {
+        expectedX = 1;
+        var x:number = calculateUserCoordinate(expectedX);
+        userCoordinateRange.forEach(offset => {
+            var coordinate = new Coordinates.UserCoordinate(x + offset, 10, size);
+            expect(coordinate.getX()).toBe(expectedX);
+        });
+    })
+    it("expects getX to return 3", function() {
+        expectedX = 3;
+        var x:number = calculateUserCoordinate(expectedX);
+        userCoordinateRange.forEach(offset => {
+            var coordinate = new Coordinates.UserCoordinate(x + offset, 10, size);
+            expect(coordinate.getX()).toBe(expectedX);
+        });
+    })
+});
