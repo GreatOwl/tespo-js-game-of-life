4d59717a5a630dc3b6b7ec422b0da8ac39f92545
Andrew Nowak <andrew@nowakfamily.net>
2018-09-24 14:26:32 -0400

    test that string length coordinates returned from y axis

--- src/spec/Coordinate.spec.ts
+++ src/spec/Coordinate.spec.ts
@@ -1,65 +1,65 @@
 import Coordinates from "../Coordinate.js";
 
-describe("Base Coordinate is reliable", function() {
+describe("Base Coordinate is reliable", () => {
     var expectedX:number;
     var expectedY:number = 7;
-    it("expects getX to return 1", function() {
+    it("expects getX to return 1", () => {
         var expectedX = 1;
         var coordinate = new Coordinates.Coordinate(expectedX, expectedY);
         expect(coordinate.getX()).toBe(expectedX);
     });
-    it("expects getX to return 3", function() {
+    it("expects getX to return 3", () => {
         var expectedX = 3;
         var coordinate = new Coordinates.Coordinate(expectedX, expectedY);
         expect(coordinate.getX()).toBe(expectedX);
     });
-    it("expects getY to return 7", function() {
+    it("expects getY to return 7", () => {
         var expectedX = 3;
         var coordinate = new Coordinates.Coordinate(expectedX, expectedY);
         expect(coordinate.getY()).toBe(expectedY);
     });
-    it("expects getY to return 5", function() {
+    it("expects getY to return 5", () => {
         var expectedX = 3;
         expectedY = 5;
         var coordinate = new Coordinates.Coordinate(expectedX, expectedY);
         expect(coordinate.getY()).toBe(expectedY);
     });
-    it("expects toString to return '0000300005'", function() {
+    it("expects toString to return '0000300005'", () => {
         var expectedString:string = "0000300005";
         var coordinate = new Coordinates.Coordinate(3, 5);
         expect(coordinate.toString()).toBe(expectedString);
     });
-    it("expects toString to return '0000400007'", function() {
+    it("expects toString to return '0000400007'", () => {
         var expectedString:string = "0000400007";
         var coordinate = new Coordinates.Coordinate(4, 7);
         expect(coordinate.toString()).toBe(expectedString);
     });
-    it("expects toString to return '0004400037'", function() {
+    it("expects toString to return '0004400037'", () => {
         var expectedString:string = "0004400037";
         var coordinate = new Coordinates.Coordinate(44, 37);
         expect(coordinate.toString()).toBe(expectedString);
     });
-    it("expects toString to return '5004450037'", function() {
+    it("expects toString to return '5004450037'", () => {
         var expectedString:string = "5004450037";
         var coordinate = new Coordinates.Coordinate(50044, 50037);
         expect(coordinate.toString()).toBe(expectedString);
     });
-    it("expects toString to return truncated string '5004450037'", function() {
+    it("expects toString to return truncated string '5004450037'", () => {
         var expectedString:string = "5004450037";
         var coordinate = new Coordinates.Coordinate(550044, 550037);
         expect(coordinate.toString()).toBe(expectedString);//.not(expectedString);
     });
   });
-describe("User Coordinate successfully converts to Base Coordinate", function() {
+describe("User Coordinate successfully converts to Base Coordinate", () => {
     var userCoordinateRange:number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     var expectedX:number;
     var expectedY:number = 7;
     var cursorFudgeSize: number = 10;
     var size: number = userCoordinateRange.length;
-    var calculateUserCoordinate =  function(expected: number):number {
+    var calculateUserCoordinate = (expected: number):number => {
         return expected * size + cursorFudgeSize
     }
-    it("expects getX to return 1", function() {
+    it("expects getX to return 1", () => {
         expectedX = 1;
         var x:number = calculateUserCoordinate(expectedX);
         userCoordinateRange.forEach(offset => {
@@ -67,7 +67,7 @@ describe("User Coordinate successfully converts to Base Coordinate", function() 
             expect(coordinate.getX()).toBe(expectedX);
         });
     })
-    it("expects getX to return 3", function() {
+    it("expects getX to return 3", () => {
         expectedX = 3;
         var x:number = calculateUserCoordinate(expectedX);
         userCoordinateRange.forEach(offset => {
@@ -75,4 +75,32 @@ describe("User Coordinate successfully converts to Base Coordinate", function() 
             expect(coordinate.getX()).toBe(expectedX);
         });
     })
+    it("expects getY to return 7", () => {
+        var y:number = calculateUserCoordinate(expectedY);
+        userCoordinateRange.forEach(offset => {
+            var coordinate = new Coordinates.UserCoordinate(10, y + offset, size);
+            expect(coordinate.getY()).toBe(expectedY);
+        });
+    })
+    it("expects getY to return 5", () => {
+        expectedY = 5;
+        var y:number = calculateUserCoordinate(expectedY);
+        userCoordinateRange.forEach(offset => {
+            var coordinate = new Coordinates.UserCoordinate(10, y + offset, size);
+            expect(coordinate.getY()).toBe(expectedY);
+        });
+    })
+    it("expects toString to return '0000300005'", () => {
+        expectedX = 3;
+        expectedY = 5;
+        var x:number = calculateUserCoordinate(expectedX);
+        var y:number = calculateUserCoordinate(expectedY);
+        userCoordinateRange.forEach(offsetX => {
+            userCoordinateRange.forEach(offsetY => {
+                var coordinate = new Coordinates.UserCoordinate(x + offsetX, y + offsetY, size);
+                expect(coordinate.getX()).toBe(expectedX);
+                expect(coordinate.getY()).toBe(expectedY);
+            });
+        });
+    });
 });
