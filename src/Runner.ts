84b8b6fb20a7f1165a40ba6f4a04f7b69e0ce295
Andrew Nowak <andrew@nowakfamily.net>
2018-09-27 18:07:15 -0400

    find and fix a bug in the rules

--- src/Runner.ts
+++ src/Runner.ts
@@ -10,31 +10,14 @@ export module Runner {
                 alive:[
                     {name:"live with fewer than 2 live neighbors dies", rule:(cell:N.Neighbor):N.Neighbor => {
                         if (cell.isAlive()) {
-                            if (cell.count() >= 2) {
+                            if (cell.count() >= 2 && cell.count() <= 3) {
                                 return cell;
                             }
                             return null;
-                        }
-                        return null;
-                    }},
-                    {name:"live with more than 3 live neighbors dies", rule:(cell:N.Neighbor):N.Neighbor => {
-                        if (cell.isAlive()) {
-                            if (cell.count() < 4) {
+                        } else {
+                            if (cell.count() == 3) {
                                 return cell;
                             }
-                            return null;
-                        }
-                        return null;
-                    }}
-                ],
-                dead: [
-                    {name:"dead with exactly 3 live neighbors spawns", rule:(cell:N.Neighbor):N.Neighbor => {
-                        if (!cell.isAlive()) {
-                            if (cell.count() === 3) {
-                                cell.zombify();
-                                return cell;
-                            }
-                            return null;
                         }
                         return null;
                     }}
@@ -46,7 +29,6 @@ export module Runner {
         return () => {
             var cells:Map<string, N.Neighbor> = new Map(scanner.scan())
             var rules:rule[] = game.original.rules.alive;
-            var deadRules:rule[] = game.original.rules.dead;
             grid.clearGrid(universe);
             cells.forEach((cell:N.Neighbor) => {
                 var ruleRunner = (rule) => {
@@ -61,8 +43,7 @@ export module Runner {
                     }
                     cell = tempCell;
                 }
-                rules.forEach(ruleRunner)
-                deadRules.forEach(ruleRunner)
+                ruleRunner(rules[0]);
             })
             return runOnce(grid, new S.Scanner(grid, universe), universe);
         }
